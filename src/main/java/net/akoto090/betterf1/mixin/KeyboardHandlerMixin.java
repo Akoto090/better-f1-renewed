96709b0bfc382c0b4149e0697d20a7ec2e432aba
Akoto090 <hashtaggurke11@gmail.com>
2025-05-16 19:27:46 +0200

    Add: refmap

--- src/main/java/net/akoto090/betterf1/mixin/KeyboardHandlerMixin.java
+++ src/main/java/net/akoto090/betterf1/mixin/KeyboardHandlerMixin.java
@@ -1,17 +1,17 @@
 package net.akoto090.betterf1.mixin;
 
 import net.akoto090.betterf1.BetterF1Mod;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.KeyboardHandler;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+
 @Mixin(KeyboardHandler.class)
 public class KeyboardHandlerMixin {
     @Inject(
-            method = "keyPress",
+            method = "m_90893_",
             at = @At("HEAD"),
             cancellable = true
     )
